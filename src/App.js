67c05e60de04a8227c64ed095f46e931b3071420
RashadXalil <rashadmkh@code.edu.az>
2022-09-14 16:19:16 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [age, SetAge] = useState(0)
+  const [users, setUsers] = useState([])
+  let index = 0
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="wrapper">
+        <div className="form__item">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="name"
+            onChange={(e) => {
+              setFirstName(e.target.value)
+            }}
+          />
+        </div>
+        <div className="form__item">
+          <label htmlFor="surname">Surname</label>
+          <input
+            type="text"
+            id="surname"
+            placeholder="Surname"
+            onChange={(e) => {
+              setLastName(e.target.value)
+            }}
+          />
+        </div>
+        <div className="form__item">
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            id="age"
+            placeholder="age"
+            onChange={(e) => {
+              SetAge(e.target.value)
+            }}
+          />
+        </div>
+        <button
+          onClick={() => {
+            let user = {
+              id: index,
+              Username: firstName,
+              Usersurname: lastName,
+              Userage: age,
+            }
+            setUsers([...users, user])
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      <ul>
+        {users.map((user, key) => {
+          return (
+            <li
+              onClick={(e) => {
+                console.log(e.target.id)
+              }}
+              id={key}
+              key={key}
+            >
+              {user.Username} {user.Usersurname} {user.Userage}
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
